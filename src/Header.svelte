306da6360d7b873c9ccf637791fa437a05896aae
bmehder <bmehder@gmail.com>
2023-06-01 18:38:05 +0200

    merged global styles

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,23 +1,23 @@
 <script>
-	export let value
-	export let endpoints
+  export let value
+  export let endpoints
 </script>
 
 <header>
-	<h1>FlipCards</h1>
-	<select bind:value>
-		{#each endpoints as endpoint}
-			<option value="{endpoint.url}">{endpoint.name}</option>
-		{/each}
-	</select>
+  <h1>FlipCards</h1>
+  <select bind:value>
+    {#each endpoints as endpoint}
+      <option value={endpoint.url}>{endpoint.name}</option>
+    {/each}
+  </select>
 </header>
 
 <style>
-	header {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-		gap: var(--size);
-		padding-inline: var(--size);
-	}
+  header {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    gap: var(--size);
+    padding-inline: var(--size);
+  }
 </style>
--- src/endpoints.js
+++ src/endpoints.js
@@ -1,14 +1,14 @@
 export default [
-	{
-		name: 'Art Class',
-		url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=art-class'
-	},
-	{
-		name: 'STEAM Club',
-		url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=steam'
-	},
-	{
-		name: 'No',
-		url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=stea'
-	},
+  {
+    name: 'Art Class',
+    url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=art-class',
+  },
+  {
+    name: 'STEAM Club',
+    url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=steam',
+  },
+  {
+    name: 'No',
+    url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=stea',
+  },
 ]
