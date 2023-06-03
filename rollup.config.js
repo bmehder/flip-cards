0b6e66db9a8ff25680032a69d098dc50cf8dd012
bmehder <bmehder@gmail.com>
2023-06-03 14:46:47 +0200

    made the app embeddable

--- rollup.config.js
+++ rollup.config.js
@@ -34,8 +34,8 @@ export default {
 	output: {
 		sourcemap: true,
 		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
+		name: 'flipCards',
+		file: 'public/build/flip-cards.js'
 	},
 	plugins: [
 		svelte({
@@ -46,7 +46,7 @@ export default {
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: 'flip-cards.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
 import App from './App.svelte';
 
-var app = new App({
-  target: document.body,
-  props: {
-    endpoints: [
-      {
-        name: 'Art Class',
-        url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=art-class',
-      },
-      {
-        name: 'STEAM Club',
-        url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=steam',
-      },
-      {
-        name: 'No',
-        url: 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=stea',
-      },
-    ],
-  },
-})
-
-export default app;
+export default function (target, props, anchor = null) {
+  new App({
+    target,
+    props,
+    anchor,
+  })
+}
